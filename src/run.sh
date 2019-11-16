55c6e707f9e5a05596d48c418c18e113dd26773a
WalkerSumida <walkersumida@gmail.com>
2019-11-16 15:36:46 +0900

    docs: add comments

--- src/run.sh
+++ src/run.sh
@@ -1,3 +1,8 @@
+# cargo-edit is a utility for managing cargo dependencies from the command line.
+#   cargo install cargo-edit
+#   e.g.
+#     cargo add cross --build
+
 cargo install --force --path .
 
 hello_world
